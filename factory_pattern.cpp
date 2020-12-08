3e4a5c744d8eda3d7b904c68ded6b7f36f97a7e3
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2020-12-08 19:40:12 +0100

    Update factory_pattern.cpp

--- factory_pattern.cpp
+++ factory_pattern.cpp
@@ -9,6 +9,8 @@ int print(std::string input){
 
 class Fruit {
         public:
+            static Fruit *harvest_fruit(int choice);
+
             virtual void information(){
                     print("This is my Fruit info");
                 };
@@ -16,7 +18,7 @@ class Fruit {
                     print("Planting seeds!");
                 };
             Fruit() {
-                std::cout << " A Fruit is being created" << std::endl;
+                std::cout << "A Fruit is being created" << std::endl;
             }  // Constructor
             // ~Fruit(); // Destructor
     };
@@ -24,9 +26,55 @@ class Fruit {
 
 class Strawberry: public Fruit {
         public:
-            
-        
+            void plant_seeds() {
+                    print("Seeds are dispersed from my flesh");
+                }
     };
+
+class Apple: public Fruit {
+        public:
+            void plant_seeds() {
+                    print("Seeds are dispersed from my flesh");
+                }
+    };
+
+class Melon: public Fruit {
+        public:
+            void plant_seeds() {
+                    print("Seeds are dispersed from my flesh");
+                }
+    };
+
+class Orange: public Fruit {
+        public:
+            void plant_seeds() {
+                    print("Seeds are dispersed from my flesh");
+                }
+    };
+
+enum Fruit_Types {
+        apple = 1,
+        strawberry = 2,
+        melon = 3,
+        orange = 4
+    };
+
+Fruit *Fruit::harvest_fruit(int choice) {
+        switch (choice) {
+            case apple:
+                print("case 1");
+                return new Apple;
+            case strawberry:
+                return new Strawberry;
+            case melon:
+                return new Melon;
+            case orange:
+                return new Orange;
+            default:
+                return new Apple;
+        }
+
+    }
 
 int main()
 {
