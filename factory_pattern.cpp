cab182427847053f75fdd352bd658bc366c80873
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2020-12-08 17:52:49 +0100

    Update factory_pattern.cpp

--- factory_pattern.cpp
+++ factory_pattern.cpp
@@ -2,28 +2,36 @@
 #include <iostream>
 #include <string>
 
+int print(std::string input){
+            std::cout << input << std::endl;
+            return 0;
+        }
+
 class Fruit {
         public:
-            virtual void tell();
+            virtual void information(){
+                    print("This is my Fruit info");
+                };
+            virtual void plant_seeds() {
+                    print("Planting seeds!");
+                };
             Fruit() {
                 std::cout << " A Fruit is being created" << std::endl;
             }  // Constructor
             // ~Fruit(); // Destructor
     };
-    
+
+
 class Strawberry: public Fruit {
         public:
             
-            void tell(){
-                    std::cout << "hi";
-                    
-                }
+        
     };
-
 
 int main()
 {
     Strawberry berry;
-    berry.tell();
+    berry.plant_seeds();
+    berry.information();
     return 0;
 }
