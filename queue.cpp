38553758cb5138ba72dc0338d4aa799e9abdb352
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2020-12-09 21:13:08 +0100

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <string>
 
@@ -36,6 +44,8 @@ class Queue {
             content_amount = 0;
         }
         
+        int count () { return content_amount; }
+        
         // Add node to the back of queue
         void append(Node* new_node) {
             Node* temp = new_node;
@@ -46,8 +56,7 @@ class Queue {
         }
         
         Node* pop() {
-            if (!(content_amount > 0)){}
-            else {
+            if (content_amount > 1) {
                 // Take out the front, set the next node as front
                 Node* cur_node;
                 Node* old_front; // The front that is being ejected
@@ -57,11 +66,13 @@ class Queue {
                 if (cur_node->next != nullptr) {
                     while (cur_node->next->next != nullptr) {
                         // Iterate until we get the next.next 
-                        cur_node = back->next;
+                        cur_node = cur_node->next;
                     }
                 
                     old_front = cur_node->next;
                     new_front = cur_node;
+                    
+                    new_front->next = nullptr;
                     
                     front = new_front;
                     
@@ -69,8 +80,20 @@ class Queue {
                     
                     return old_front;
                 }
-                else { return cur_node; }
+                else 
+                { 
+                    front = cur_node;
+                    return cur_node; 
+                }
                 
+            }
+            else {
+                if (content_amount == 1) {
+                    front = back;
+                    content_amount--;
+                    
+                    return front;
+                }
             }
             
             return nullptr;
@@ -84,11 +107,24 @@ int main()
     
     Node* first = new Node("one");
     Node* sec = new Node("two");
+    Node* tre = new Node("three");
+    Node* four = new Node("four");
+
+    q1.append(sec);
+    
+    q1.append(tre);
+    Node* out = q1.pop();
+    
+    cout << out->name << endl;
+    cout << q1.pop()->name << endl;
 
     q1.append(first);
-    
-    Node* out = q1.pop();
-    cout << out->name << endl;
+    cout << q1.pop()->name << endl;
+
+    cout << q1.count() << endl;
+    q1.append(four);
+    cout << q1.pop()->name << endl;
+    cout << q1.count() << endl;
 
     return 0;
 }
