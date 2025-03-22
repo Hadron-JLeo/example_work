e8d4cb9cc7dd5128975e6c8c417eb6dffb58fa0f
JLeo <67970516+Hadron-JLeo@users.noreply.github.com>
2025-03-22 17:59:00 +0100

    Create python_comments.py

---
+++ notes/python_comments.py
@@ -0,0 +1,10 @@
+def test_function(p1, p2, p3):
+    """
+    test_function does blah blah blah.
+
+    :param p1: describe about parameter p1
+    :param p2: describe about parameter p2
+    :param p3: describe about parameter p3
+    :return: describe what it returns
+    """ 
+    pass
